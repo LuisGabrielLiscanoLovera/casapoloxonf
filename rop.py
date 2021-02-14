a84f7ce8bc2dd986e10c465c0354570aaf6e978e
LuisGabrielLiscanoLovera <ccidbcomputacion12@gmail.com>
2021-02-13 23:20:52 -0500

    Add files via upload

--- rop.py
+++ rop.py
@@ -1,16 +1,20 @@
 try:
     import os
-    from flask import Flask, jsonify, make_response,url_for
-    from flask import redirect
-    from flask import render_template
+    from flask import Flask
+    from flask import jsonify
+    from flask import url_for
+    from flask import flash
+    from flask import session
+    from flask import abort
     from flask import request
-    import json
+    from flask import render_template, redirect
     from flask_sqlalchemy import SQLAlchemy
     from datetime import datetime as DT,timedelta
     from random import choice
     from flask_cors import CORS, cross_origin
-    from flask import  flash, session, abort
+    
 except Exception as e:print("Failed to import "+str(e))
+
 try:
     project_dir = os.path.dirname(os.path.abspath(__file__))
     database_file = "sqlite:///{}".format(os.path.join(project_dir, "polodb.db"))
@@ -20,7 +24,8 @@ try:
     app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=120)
     dt=DT.now()
     db = SQLAlchemy(app)
-except Exception as e:print("Failed in values"+str(e))
+except Exception as e:
+    print("Failed in values"+str(e))
 
 
 def gerar_token(tamanho):
@@ -35,6 +40,17 @@ class Users(db.Model):
     email           =db.Column(db.String(56),nullable=False)
     password        =db.Column(db.String(56),nullable=False)
     contact         =db.Column(db.String(56),nullable=False)
+    id_role         =db.Column(db.String(56))
+
+class Integrnte(db.Model):
+    id_integrante      =db.Column(db.Integer,   unique=True, nullable=False, primary_key=True)
+    nom_integrante     =db.Column(db.String(56),nullable=False)
+    email              =db.Column(db.String(56))
+    cedula             =db.Column(db.String(56))
+    contact            =db.Column(db.String(56))
+    fecha              =db.Column(db.String(56))
+    direcion           =db.Column(db.String(56))
+    foto               =db.Column(db.String(56))
 
 class Prenda(db.Model):
     id_prenda   =db.Column(db.Integer,    unique=True, nullable=False, primary_key=True)
@@ -49,14 +65,12 @@ class Prenda(db.Model):
     cant_tallaL =db.Column(db.Integer)
     cant_tallaXL =db.Column(db.Integer)
     cant_tallaXXL=db.Column(db.Integer)
-    
     rS  =db.Column(db.Integer)
     rM  =db.Column(db.Integer)
     rL  =db.Column(db.Integer)
     rXL =db.Column(db.Integer)
     rXXL=db.Column(db.Integer)
     nota        =db.Column(db.String(56))
-    
     def __repr__(self):
         #return self.op
         return "<OP: {}>".format(self.op)
@@ -69,8 +83,24 @@ class Operacion(db.Model):
     can_terminada = db.Column(db.Integer)
     can_resta     = db.Column(db.Integer)
 
+class Tarea(db.Model):
+    id_tarea      = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
+    nom_tarea     = db.Column(db.String(56))
+    fecha         = db.Column(db.DateTime)
+    duracion      = db.Column(db.String(56))
+    precio        = db.Column(db.Integer)
+    mininoDia     = db.Column(db.Integer)
+
     def __repr__(self):
-        return "<Title: {}>".format(self.id_operacion)
+        return "<Title: {}>".format(self.nom_tarea)
+class Destajo(db.Model):
+    id_prenda     = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
+    id_tarea      = db.Column(db.Integer)
+    id_integrante = db.Column(db.Integer)
+    cantidad      = db.Column(db.Integer)
+    fecha         = db.Column(db.DateTime)
+    id_talla      = db.Column(db.Integer)
+    can_resta     = db.Column(db.Integer)
 
 class Talla(db.Model):
     id_talla = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
@@ -83,7 +113,6 @@ def ct(id_prenda):
     operacion = db.engine.execute('select * from operacion where id_prenda ={};'.format(int(id_prenda)))
     sumaTotal = db.engine.execute('select sum(can_terminada) as suma from operacion where id_prenda ={};'.format(id_prenda))
     sumaTotalR =db.engine.execute('select sum(can_resta) as sumaR from operacion where id_prenda ={};'.format(id_prenda))
-    
     sct=()
     ct=()
     fec=None
@@ -92,7 +121,6 @@ def ct(id_prenda):
     idop=()
     histo=()
     rt=0
-    
     for row in operacion:
         lidop=list(idop)
         lidop.append(row.id_operacion)
@@ -102,7 +130,6 @@ def ct(id_prenda):
         ct=tuple(lct)
         fec=str('Fecha cierre:')+(str(row.fecha))[:16].translate({ord(' '): ' / '})+' <i class="icofont-clock-time"></i>'
 
-        
         tallas   = db.engine.execute('select nom_talla from talla where id_talla ={};'.format(row.id_talla))
         tallasT  = db.engine.execute('select sum(can_terminada) as tallTT from operacion where id_prenda ={} AND id_talla={};'.format(row.id_prenda,row.id_talla))
         tallasTR = db.engine.execute('select sum(can_resta) as tallTR from operacion where id_prenda ={} AND id_talla={};'.format(row.id_prenda,row.id_talla))
@@ -126,7 +153,6 @@ def ct(id_prenda):
                         lhisto.append(str("<br>"+str(row.nom_talla)+" = "+str(roww.tallTT)))
                         tllT = tuple(set(ltllT))
                         histo=tuple(set(lhisto))
-          
     for row in sumaTotal:
         for roww in sumaTotalR:
             lsct = list(sct)
@@ -140,7 +166,6 @@ def ct(id_prenda):
                     rt=int(row.suma-roww.sumaR)
                 except Exception as e:
                     pass
-    
     return {'ct':ct,'tll':tll,'tllT':tllT,'fecp':fec,'sct':sct,'rt':rt,'idop':idop,'histo':histo}
 
 def vacio(vacio):
@@ -184,7 +209,6 @@ def getData():
         return render_template("login.html")
     else:
         prenda = Prenda.query.all()
-        
         datas = {"draw": 1,
           "recordsTotal": len(prenda),
           "recordsx`x`Filtered": len(prenda)}
@@ -198,35 +222,6 @@ def getData():
         for row  in prenda:
             row.op=row.op.upper()
             row.referencia=row.referencia.upper()
-            canFalt=row.cant_total-ct(row.id_prenda)['rt']
-            S  =row.rS
-            M  =row.rM
-            L  =row.rL
-            XL =row.rXL
-            XXL=row.rXXL
-            if type(S)==str:S=0
-            if type(M)==str:M=0
-            if type(L)==str:L=0
-            if type(XL)==str:XL=0
-            if type(XXL)==str:XXL=0
-            if canFalt  ==0:canFalt=('<d class="text-info">(Completa!) <i class="icofont-verification-check"></i></d>')
-            elif canFalt  <0:canFalt =("<d> Se pasa por ("+str(canFalt*-1)  +")"+'<i class="icofont-exclamation-tringle"></i></d>')   
-            else:canFalt=('<d class="text-danger">(falta : '+str(canFalt)+') <i class="icofont-error"></i></d>')
-            if S  ==0:S  =('<d class="text-info">----------</d>')
-            elif S  <0:S =("<d> Se pasa por ("+str(S*-1)  +")"+'<i class="icofont-exclamation-tringle"></i></d>')
-            else:S  =('<d class="text-danger">(falta : '+str(S)+') <i class="icofont-error"></i></d>')
-            if M  ==0:M  =('<d class="text-info">----------</d>')
-            elif M  <0:M =("<d> Se pasa por ("+str(M*-1)  +")"+'<i class="icofont-exclamation-tringle"></i></d>')
-            else:M  =('<d class="text-danger">(falta : '+str(M)+') <i class="icofont-error"></i></d>')
-            if L  ==0:L  =('<d class="text-info">----------</d>')
-            elif L  <0:L =("<d> Se pasa por ("+str(L*-1)  +")"+'<i class="icofont-exclamation-tringle"></i></d>')
-            else:L  =('<d class="text-danger">(falta : '+str(L)+') <i class="icofont-error"></i></d>')
-            if XL ==0:XL =('<d class="text-info">----------</d>')
-            elif XL  <0:XL =("<d> Se pasa por ("+str(XL*-1)  +")"+'<i class="icofont-exclamation-tringle"></i></d>')
-            else:XL  =('<d class="text-danger">(falta : '+str(XL)+') <i class="icofont-error"></i></d>')
-            if XXL==0:XXL=('<d class="text-info">----------</d>')
-            elif XXL  <0:XXL =("<d> Se pasa por ("+str(XXL*-1)  +")"+'<i class="icofont-exclamation-tringle"></i></d>')
-            else:XXL  =('<d class="text-danger">(falta : '+str(XXL)+') <i class="icofont-error"></i></d>')
             if type(row.cant_tallaS)   ==str:row.cant_tallaS  =0
             if type(row.cant_tallaM)   ==str:row.cant_tallaM  =0
             if type(row.cant_tallaL)   ==str:row.cant_tallaL  =0
@@ -250,7 +245,7 @@ def getData():
                 estado='<d class="h6 text-info">Abierto<i class="icofont-automation"></i></d>'
                 opD='<d class="text-info icofont-error">'+str(i)+')  '+str(row.op)+'</d>'
                 fec=''
-            
+
             fecha=(str(row.fecha))[:16].translate({ord(' '): ' / '})+' <i class="icofont-clock-time"></i>'
             datas['data'].append({
           "id_pr":"{}".format(int(row.id_prenda)),
@@ -267,27 +262,20 @@ def getData():
           "tallaL": "{}".format(vacio(row.cant_tallaL)),
           "tallaXL": "{}".format(vacio(row.cant_tallaXL)),
           "tallaXXL": "{}".format(vacio(row.cant_tallaXXL)),
-          "rS": "{}".format(S),
-          "rM": "{}".format(M),
-          "rL": "{}".format(L),
-          "rXL": "{}".format(XL),
-          "rXXL": "{}".format(XXL),
           "totalTalla":"{}".format(int(TT)),
           "nota": "{}".format(row.nota),
           "tallas":ct(row.id_prenda)['tll'],
           "tallaS":ct(row.id_prenda)['tllT'],
           "total":ct(row.id_prenda)['sct'],
           "feechaOperacion": fec,
-          "canFaltante": "{}".format(canFalt),
           "estado": estado,
           "noIgual":noIgual,
           "historia":ct(row.id_prenda)['histo'],
 })
         abuelo=jsonify(datas)
     #print (datas)
-    return (abuelo)
-
-@app.route('/')
+    return abuelo
+app.route('/')
 def home():
     if 1==5:#not session.get("logged_in"):
         return render_template("login.html")
@@ -381,7 +369,7 @@ def operacion():
                 prenda        = Prenda.query.filter_by(id_prenda=request.form.get("id_prenda")).first()
                 decremt       = request.form.get("resta")
 #                oper          = Operacion.query.filter_by(id_prenda=request.form.get("id_prenda")).first()
-                
+
                 if str(decremt)=="resta":
                     can_resta     = request.form.get("can_terminada")
                     can_terminada = 0
@@ -415,7 +403,6 @@ def operacion():
             except Exception as e:
                 flash('error en la operacion')
                 print(e)
-  
     return redirect(url_for('registro'))
 
 
@@ -485,7 +472,7 @@ def delete():
         except Exception as e:
             flash('error en la operacion no se pudo borrar!')
             print(e)
-    
+
     return redirect("/registro")
 
 @app.route("/logout")
@@ -501,15 +488,14 @@ def background_process():
     else:
         if request.args:
             try:
-                id_prenda=request.args.get("id_prenda", 0, type=int)
+                id_prenda     = request.args.get("id_prenda", 0, type=int)
                 can_terminada = request.args.get("can_terminada", 0, type=int)
                 id_talla      = request.args.get("id_talla", 0, type=int)
                 prenda        = Prenda.query.filter_by(id_prenda=id_prenda).first()
                 decremt       = request.args.get("resta", 0, type=int)
 #                oper          = Operacion.query.filter_by(id_prenda=request.form.get("id_prenda")).first()
-                print (id_talla)
                 if str(decremt)=="resta":
-                    can_resta     = request.args.get("can_terminada",0, type=int)
+                    can_resta     = can_terminada
                     can_terminada = 0
                     if id_talla==1:prenda.rS  =(prenda.rS+int(can_resta))
                     if id_talla==2:prenda.rM  =(prenda.rM+int(can_resta))
@@ -518,7 +504,7 @@ def background_process():
                     if id_talla==5:prenda.rXXL=(prenda.rXXL+int(can_resta))
 
                 else:
-                    can_terminada = request.args.get("can_terminada",0, type=int)
+                    can_terminada = can_terminada
                     can_resta     = 0
                     if id_talla==1:prenda.rS  =(prenda.rS-int(can_terminada))#db.engine.execute ('UPDATE prenda set rS = 1 where id_prenda=1;')
                     if id_talla==2:prenda.rM  =(prenda.rM-int(can_terminada))
@@ -526,35 +512,70 @@ def background_process():
                     if id_talla==4:prenda.rXL =(prenda.rXL-int(can_terminada))
                     if id_talla==5:prenda.rXXL=(prenda.rXXL-int(can_terminada))
                 operacion     = Operacion(fecha  =dt,
-                                   id_prenda     = request.args.get("id_prenda",0, type=int),
+                                   id_prenda     = id_prenda,
                                    can_terminada = can_terminada,
                                    can_resta     = can_resta,
-                                   id_talla      = request.args.get("id_talla",0, type=int))        
-                
+                                   id_talla      = id_talla)
                 if id_talla=="Seleccione Talla" or id_talla=="None" or can_terminada=="None" or can_terminada=="" or id_prenda=="None":pass
                 else:
                     db.session.add(prenda)
                     db.session.add(operacion)
                     db.session.commit()
-                        
             except Exception as e:
                 flash('error en la operacion')
                 print(e)
+    #datas = getData()['datas']
     return jsonify(result='id_talla:'+str(id_talla)+' cantidad:'+str(can_terminada))
- #   return redirect(url_for('registro'))    
 
+@app.route('/getDataFaltante/<id_prenda>')
+def getDataFaltante(id_prenda):
+    prenda   = db.engine.execute('select rS, rM, rL, rXL, rXXL , op, cant_total from prenda where id_prenda ={};'.format(id_prenda))
+    
+    for i in prenda:
+        rS=i.rS
+        rM=i.rM
+        rL=i.rL
+        rXL=i.rXL
+        rXXL=i.rXXL
 
+        canFalt=i.cant_total-ct(id_prenda)['rt']
+        if canFalt  ==0:canFalt='(Completa!)'
+        elif canFalt  <0:canFalt ="Se pasa por ("+str(canFalt*-1)  +")"   
+        else:canFalt='falta : '+str(canFalt)
 
+        if rS  ==0:rS  ='----------'
+        elif rS  <0:rS ="Se pasa por ("+str(rS*-1)  +")"
+        else:rS  ='falta : '+str(rS)+')'
+        if rM  ==0:rM  ='----------'
+        elif rM <0:rM ="Se pasa por ("+str(rM*-1)  +")"
+        else:rM  ='falta : '+str(rM)+')'
+        if rL  ==0:rL  =('----------')
+        elif rL  <0:rL ="Se pasa por ("+str(rL*-1)  +")"
+        else:rL  ='falta : '+str(rL)+')'
+        if rXL ==0:rXL ='----------'
+        elif rXL  <0:rXL =" Se pasa por ("+str(rXL*-1)  +")"
+        else:rXL  ='falta : '+str(rXL)+')'
+        if rXXL==0:rXXL='----------'
+        elif rXXL  <0:rXXL ="Se pasa por ("+str(rXXL*-1)  +")"
+        else:rXXL  ='falta : '+str(rXXL)+')'    
+    return jsonify(megaRS=str(rS),
+        megaRM=str(rM),
+        megaRL=str(rL),
+        megaRXL=str(rXL),
+        megaRXXL=str(rXXL),
+        megacanFalt=str(canFalt)
+        )
+        #result ='id_talla:'+str(id_talla)+' cantidad:'+str(can_terminada)
+        #return jsonify(prenda)
 
-@app.route('/interactive/')
-def interactive():
-    return render_template('interactive.html')
-
+        
+        
+     
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.secret_key =gerar_token(8)
+    app.secret_key = gerar_token(8)
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.run(host='127.0.0.1', port=5000)
